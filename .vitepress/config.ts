a8aef19f253cd24bc2a9a522b564c6566651dc18
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-01-20 22:03:41 +0530

    feat: use patterns in rewrites

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  remap: {
-    'packages/package-b/src/index.md': '-/package-b/index.md',
-    'packages/package-b/src/config.md': '-/package-b/config.md',
-    'packages/package-a/src/index.md': '-/package-a/index.md',
-    'packages/package-a/src/config.md': '-/package-a/config.md',
-    'docs/index.md': 'index.md',
-    'docs/globals.md': 'globals.md'
+  rewrites: {
+    'packages/:package/src/:splat*': '-/:package/:splat*',
+    'docs/:splat*': ':splat*'
   }
 })
